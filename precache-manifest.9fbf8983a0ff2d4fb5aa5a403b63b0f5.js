0024590ce4e2d0c6c66b0025d08a81a19cc2e658
Julio Benavente <32945829+julio-benavente@users.noreply.github.com>
2020-10-08 12:04:32 -0500

    Updates

---
+++ precache-manifest.9fbf8983a0ff2d4fb5aa5a403b63b0f5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcbb10d07f56664d54d33045daa90a7",
+    "url": "/learningmangsys/index.html"
+  },
+  {
+    "revision": "c7afc0731eec445cea45",
+    "url": "/learningmangsys/static/css/main.c5a4ed9b.chunk.css"
+  },
+  {
+    "revision": "960c3b3d757ca4afccfe",
+    "url": "/learningmangsys/static/js/2.f37df932.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/learningmangsys/static/js/2.f37df932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7afc0731eec445cea45",
+    "url": "/learningmangsys/static/js/main.5a5dc38b.chunk.js"
+  },
+  {
+    "revision": "24fee4f259a2189da760",
+    "url": "/learningmangsys/static/js/runtime-main.8ad2091f.js"
+  },
+  {
+    "revision": "16bb70256197a0b632a5f207b6fbb777",
+    "url": "/learningmangsys/static/media/005-analytics.16bb7025.svg"
+  },
+  {
+    "revision": "cfed8a178b37546b6ab6a55b339156a8",
+    "url": "/learningmangsys/static/media/013-grid.cfed8a17.svg"
+  },
+  {
+    "revision": "82024ac2a62110512b8de1f90326c50e",
+    "url": "/learningmangsys/static/media/016-creativity.82024ac2.svg"
+  },
+  {
+    "revision": "13994e71175849c0eae5a61fa8f13a0c",
+    "url": "/learningmangsys/static/media/018-launch.13994e71.svg"
+  },
+  {
+    "revision": "c0738c0c4ee29dbdc83aff40e004a85d",
+    "url": "/learningmangsys/static/media/022-rgb.c0738c0c.svg"
+  },
+  {
+    "revision": "8541b42be47d41c8cb4cd7f8d1eaa4aa",
+    "url": "/learningmangsys/static/media/035-mind.8541b42b.svg"
+  },
+  {
+    "revision": "56d060923ef3b4f22d8cc2e4349ef1da",
+    "url": "/learningmangsys/static/media/041-ideas.56d06092.svg"
+  },
+  {
+    "revision": "b530723786d5e175cc1088cf9d4c8f03",
+    "url": "/learningmangsys/static/media/browser-3.b5307237.svg"
+  },
+  {
+    "revision": "1f32baa11b3f8e6ec524ff4e92ccb5a9",
+    "url": "/learningmangsys/static/media/browser-5.1f32baa1.svg"
+  },
+  {
+    "revision": "39c0b205854ca0f304a37f35c5d86bc4",
+    "url": "/learningmangsys/static/media/folder-1.39c0b205.svg"
+  },
+  {
+    "revision": "85698e3463bc7ffb946bd32f1ce26e9f",
+    "url": "/learningmangsys/static/media/network.85698e34.svg"
+  }
+]);
